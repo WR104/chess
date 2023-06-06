0d9642d515022edb79935fee8337ca61f860fa67
WR104 <252600486L@gmail.com>
2023-06-06 01:37:29 -0700

    Update bootstrap.js

--- bootstrap.js
+++ bootstrap.js
@@ -1,4 +1,4 @@
-/******/ (function(modu123les) { // webpackBootstrap
+/******/ (function(modules) { // webpackBootstrap
 /******/ 	// install a JSONP callback for chunk loading
 /******/ 	function webpackJsonpCallback(data) {
 /******/ 		var chunkIds = data[0];
